76d360a0c8e42aa383a8d9ef535c3007de254fb3
sanyabeast <a.gvrnsk@gmail.com>
2016-12-28 21:50:22 +0200

    cell: changed inner structure.

--- js/modules/Cell.js
+++ js/modules/Cell.js
@@ -5,54 +5,70 @@ define([
     'postal'
     ], function(config, postal) {
 
-    var Cell = function( /*obj*/ options, /*obj*/ parent) {
+    var Cell = function(/*obj*/options, /*obj*/parent) {
+        this.parent = parent;
+
         this.element = document.createElement('div');
         this.element.classList.add('cell');
 
-        this.options = options;
+        this.x = options.x;
+        this.y = options.y;
+        this.color = options.color;
+        this.size = options.size;
 
         this.setSize(options.size);
         this.setColor(options.color);
-        this.setPosition(options.x, options.y);
+        this.setXY(options.x, options.y);
 
         this.element.addEventListener('click', function(evt) {
             this.onClick(evt);
         }.bind(this), false);
 
-        parent.appendChild(this.element);
+        this.state = 'default';
+
+        this.attach();
     };
 
     Cell.prototype = {
-        setPosition: function(x, y) {
-            this.element.style.left = ((100 / this.options.size) * x) + '%';
-            this.element.style.top = ((100 / this.options.size) * y) + '%';
+        /*методы для прикрепления и открпеления целла к родительскому контейнеру*/
+        attach : function(){
+            this.parent.appendChild(this.element);
+        },
+        detach : function(){
+            this.parent.removeChild(this.element);
+        },
+        /*мгновенная установка позиции*/
+        setXY: function(x, y) {
+            this.x = x;
+            this.y = y;
+
+            this.element.style.left = ((100 / this.size) * x) + '%';
+            this.element.style.top = ((100 / this.size) * y) + '%';
         },
         setSize: function(size) {
             this.element.style.width = (100 / size) + '%';
             this.element.style.height = (100 / size) + '%';
         },
-        setColor: function( /*num*/ id) {
-            this.options.color = id;
+        setColor: function(/*num*/id) {
+            this.color = id;
             this.element.style.backgroundColor = config.colors[id];
         },
-        getColor: function() {
-            return this.options.color;
+        /*состояния - добавляет к ячейке класс с именем, которое передеается в качестве аргумента
+          сбрасывая все предыдущие состояния*/
+        setState : function(/*str*/stateName){
+            this.resetState();
+            this.element.classList.add(stateName);
+            this.state = stateName;
         },
-        sameColor: function( /*obj*/ cell) {
-            return (cell.options.color === this.options.color);
-        },
-        setSelected: function( /*bool*/ isSelected) {
-            if (isSelected) {
-                this.element.classList.add('selected');
-            } else {
-                this.element.classList.remove('selected')
+        resetState : function(){
+            for (var a = 0; a < config.cellStyles; a++){
+                this.element.classList.remove(config.cellStyles[a]);
             }
+
+            this.state = 'default';
         },
-        getSelected: function() {
-            return this.element.classList.contains('selected');
-        },
-        /*on-click callback*/
-        onClick: function( /*obj*/ evt) {
+        /*действие при клике*/
+        onClick: function( /*obj*/ evt){
             postal.publish({
                 channel : 'cells',
                 topic : 'clicked',
